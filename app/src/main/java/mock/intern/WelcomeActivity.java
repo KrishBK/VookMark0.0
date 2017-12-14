b931000a9880dfbc4f20fb0694b81f0b6227e770
Bala <balakrishnan.750@gmail.com>
2017-12-14 11:00:38 +0530

    new changes

--- app/src/main/java/mock/intern/WelcomeActivity.java
+++ app/src/main/java/mock/intern/WelcomeActivity.java
@@ -1,202 +1,207 @@
 package mock.intern;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import mock.intern.OnBoardItem;
+import mock.intern.OnBoard_Adapter;
+import mock.intern.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private MyViewPagerAdapter myViewPagerAdapter;
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
-    private int[] layouts;
-    private Button btnSkip, btnNext;
-    private PrefManager prefManager;
+    private LinearLayout pager_indicator;
+    private int dotsCount;
+    private ImageView[] dots;
+
+
+
+    private ViewPager onboard_pager;
+
+    private OnBoard_Adapter mAdapter;
+
+    private Button btn_get_started;
+
+    int previous_pos=0;
+
+
+    ArrayList<OnBoardItem> onBoardItems=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
 
-        // Checking for first time launch - before calling setContentView()
-        prefManager = new PrefManager(this);
-        if (!prefManager.isFirstTimeLaunch()) {
-            launchHomeScreen();
-            finish();
-        }
+        btn_get_started = (Button) findViewById(R.id.btn_get_started);
+        onboard_pager = (ViewPager) findViewById(R.id.pager_introduction);
+        pager_indicator = (LinearLayout) findViewById(R.id.viewPagerCountDots);
 
-        // Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
+        loadData();
 
-        setContentView(R.layout.activity_welcome);
-
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-        btnSkip = (Button) findViewById(R.id.btn_skip);
-        btnNext = (Button) findViewById(R.id.btn_next);
-
-
-        // layouts of all welcome sliders
-        // add few more layouts if you want
-        layouts = new int[]{
-                R.layout.welcome_slide1,
-                R.layout.welcome_slide2,
-                R.layout.welcome_slide3,
-                R.layout.welcome_slide4};
-
-        // adding bottom dots
-        addBottomDots(0);
-
-        // making notification bar transparent
-        changeStatusBarColor();
-
-        myViewPagerAdapter = new MyViewPagerAdapter();
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        btnSkip.setOnClickListener(new View.OnClickListener() {
+        mAdapter = new OnBoard_Adapter(this,onBoardItems);
+        onboard_pager.setAdapter(mAdapter);
+        onboard_pager.setCurrentItem(0);
+        onboard_pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View v) {
-                launchHomeScreen();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
             }
-        });
 
-        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // checking for last page
-                // if last page home screen will be launched
-                int current = getItem(+1);
-                if (current < layouts.length) {
-                    // move to next screen
-                    viewPager.setCurrentItem(current);
-                } else {
-                    launchHomeScreen();
+            public void onPageSelected(int position) {
+
+                // Change the current position intimation
+
+                for (int i = 0; i < dotsCount; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(WelcomeActivity.this, R.drawable.non_selected_item_dot));
                 }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(WelcomeActivity.this, R.drawable.selected_item_dot));
+
+
+                int pos=position+1;
+
+               if(pos==dotsCount&&previous_pos==(dotsCount-1))
+                    show_animation();
+                else if(pos==(dotsCount-1)&&previous_pos==dotsCount)
+                    hide_animation();
+
+                previous_pos=pos;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
-    }
 
-    private void addBottomDots(int currentPage) {
-        dots = new TextView[layouts.length];
+        btn_get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
-
-        dotsLayout.removeAllViews();
-        for (int i = 0; i < dots.length; i++) {
-            dots[i] = new TextView(this);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-            dots[i].setTextColor(colorsInactive[currentPage]);
-            dotsLayout.addView(dots[i]);
-        }
-
-        if (dots.length > 0)
-            dots[currentPage].setTextColor(colorsActive[currentPage]);
-    }
-
-    private int getItem(int i) {
-        return viewPager.getCurrentItem() + i;
-    }
-
-    private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-        finish();
-    }
-
-    //	viewpager change listener
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageSelected(int position) {
-            addBottomDots(position);
-
-            // changing the next button text 'NEXT' / 'GOT IT'
-            if (position == layouts.length - 1) {
-                // last page. make button text to GOT IT
-                btnNext.setText(getString(R.string.start));
-                btnSkip.setVisibility(View.GONE);
-            } else {
-                // still pages are left
-                btnNext.setText(getString(R.string.next));
-                btnSkip.setVisibility(View.VISIBLE);
+                Toast.makeText(WelcomeActivity.this,"Redirect to wherever you want",Toast.LENGTH_LONG).show();
             }
-        }
+        });
 
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        setUiPageViewController();
 
-        }
+    }
 
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
+    // Load data into the viewpager
 
-        }
-    };
+    public void loadData()
+    {
 
-    /**
-     * Making notification bar transparent
-     */
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+        int[] header = {R.string.ob_header1, R.string.ob_header2, R.string.ob_header3};
+        int[] desc = {R.string.ob_desc1, R.string.ob_desc2, R.string.ob_desc3};
+        int[] imageId = {R.drawable.onboard_page1, R.drawable.onboard_page2, R.drawable.onboard_page3};
+
+        for(int i=0;i<imageId.length;i++)
+        {
+            OnBoardItem item=new OnBoardItem();
+            item.setImageID(imageId[i]);
+            item.setTitle(getResources().getString(header[i]));
+            item.setDescription(getResources().getString(desc[i]));
+
+            onBoardItems.add(item);
         }
     }
 
-    /**
-     * View pager adapter
-     */
-    public class MyViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
+    // Button bottomUp animation
 
-        public MyViewPagerAdapter() {
-        }
+    public void show_animation()
+    {
+        Animation show = AnimationUtils.loadAnimation(this, R.anim.slide_up_anim);
 
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        btn_get_started.startAnimation(show);
 
-            View view = layoutInflater.inflate(layouts[position], container, false);
-            container.addView(view);
+        show.setAnimationListener(new Animation.AnimationListener() {
 
-            return view;
-        }
+            @Override
+            public void onAnimationStart(Animation animation) {
+                btn_get_started.setVisibility(View.VISIBLE);
+            }
 
-        @Override
-        public int getCount() {
-            return layouts.length;
-        }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
 
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                btn_get_started.clearAnimation();
+
+            }
+
+        });
 
 
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View) object;
-            container.removeView(view);
-        }
     }
+
+    // Button Topdown animation
+
+    public void hide_animation()
+    {
+        Animation hide = AnimationUtils.loadAnimation(this, R.anim.slide_down_anim);
+
+        btn_get_started.startAnimation(hide);
+
+        hide.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                btn_get_started.clearAnimation();
+                btn_get_started.setVisibility(View.GONE);
+
+            }
+
+        });
+
+
+    }
+
+    // setup the
+    private void setUiPageViewController() {
+
+        dotsCount = mAdapter.getCount();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(WelcomeActivity.this, R.drawable.non_selected_item_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(6, 0, 6, 0);
+
+            pager_indicator.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(WelcomeActivity.this, R.drawable.selected_item_dot));
+    }
+
+
 }
